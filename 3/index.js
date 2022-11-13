6a9ddbb93c71f32ce54a6271a12aa633921360cc
KononyukL <liliashpak97@gmail.com>
2022-11-13 17:22:10 +0200

    fixed 3 after review

--- 3/index.js
+++ 3/index.js
@@ -1,79 +1,64 @@
 //2.
-
-// Першим аргументом може бути масив будь-яких рядків інших значень там немає (input validation)
-
 const stringToUpperCase = (item) => {
-  return item[0].toUpperCase() + item.slice(1);
+  return item.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
 };
 
 const arrayToCamelCase = (arr, callback) => {
-  const result = arr.map((elem) => {
-    return callback(elem);
-  });
-  return result.join("");
+  return arr
+    .map((elem) => {
+      return callback(elem);
+    })
+    .join("");
 };
 
 console.log(arrayToCamelCase(["my", "name", "is", "Vasya"], stringToUpperCase));
 
-// Першим аргументом може бути тільки масив будь-яких чисел
-
-const multiplyByTen = (elem) => {
-  return elem * 10;
-};
+const multiplyByTen = (elem) => elem * 10;
 
 const multiplyItemsByTen = (arr, callback) => {
-  let result = arr.map((item) => {
-    return callback(item);
-  });
-  return result.join(" , ");
+  return arr
+    .map((item) => {
+      return callback(item);
+    })
+    .join(" , ");
 };
 
 console.log(multiplyItemsByTen([10, 20, 30], multiplyByTen));
 
-//Першим аргументом тільки об'єкти такого формату
-//func([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], cb);
-// 'Jhon is 45, Aaron is 20'
-
-const userDataToString = (item) => {
-  return `${item.name} is ${item.age}`;
-};
+const userDataToString = (item) => `${item.name} is ${item.age}`;
 
 const usersListDataToString = (arr, callback) => {
-  const result = arr.map((elem) => {
-    return callback(elem);
-  });
-  return result.join(" , ");
+  return arr
+    .map((elem) => {
+      return callback(elem);
+    })
+    .join(" , ");
 };
+
 console.log(
   usersListDataToString(
     [
-      {age: 45, name: "Jhon"},
-      {age: 20, name: "Aaron"},
+      { age: 45, name: "Jhon" },
+      { age: 20, name: "Aaron" },
     ],
     userDataToString
   )
 );
 
-//Першим аргументом може бути  тільки масив рядків
-//func(['abc', '123'], cb) →
-// 'cba, 321' // рядки розвертаються
-
-const stringReverse = (item) => {
-  return item.split("").reverse().join("");
-};
+const stringReverse = (item) => item.split("").reverse().join("");
 
 const arrayItemsRevers = (arr, callback) => {
-  const result = arr.map((elem) => {
-    return callback(elem);
-  });
-  return result.join(" , ");
+  return arr
+    .map((elem) => {
+      return callback(elem);
+    })
+    .join(" , ");
 };
 
 console.log(arrayItemsRevers(["abc", "123"], stringReverse));
 
 // 3. this
 // 3.1.
-
 const rectangle = {
   width: 7,
   height: 5,
@@ -90,10 +75,10 @@ const price = {
     return this.price;
   },
   getPriceWithDiscount() {
-    const percent = (this.price / 100) * parseInt(this.discount, 10);
-    return this.price - percent;
+    return this.price - (this.price / 100) * parseInt(this.discount);
   },
 };
+
 price.getPrice(); // 10
 price.getPriceWithDiscount(); // 8.5
 
@@ -117,7 +102,6 @@ numerator.double().plusOne().plusOne().minusOne();
 numerator.value; // 3
 
 //3.4.
-
 const element = {
   height: 25,
   getHeight: function () {
@@ -128,20 +112,14 @@ const getElementHeight = element.getHeight.bind(element);
 getElementHeight();
 
 //4.
-const convertToObject = (num) => {
-  const obj = {
-    value: num,
-    isOdd: Boolean(num % 2),
-  };
-  return obj;
-};
+const convertToObject = (num) => ({
+  value: num,
+  isOdd: Boolean(num % 2),
+});
 
-// 5. Замикання
 // 5.1
+const minus = (first = 0) => (second = 0) => first - second;
 
-const minus = (firsParam = 0) => {
-  return (secondParam = 0) => firsParam - secondParam;
-};
 minus(10)(6); // 4
 minus(5)(6); // -1
 minus(10)(); // 10
@@ -149,7 +127,6 @@ minus()(6); // -6
 minus()(); // 0
 
 // 5.2
-
 function multiplyMaker(num) {
   let result = num;
 
@@ -167,79 +144,74 @@ multiply(3); // 12 (4 * 3)
 multiply(10); // 120 (12 * 10)
 
 // 5.3.
-
-(function () {
+const module = (function () {
   let string = "";
 
-  function setString(str) {
-    if (typeof str === "number" && !isNaN(str)) {
-      string = String(str);
-      return;
-    }
-    string = str || "";
-  }
+  return {
+    setString: function (str) {
+      if (typeof str === "number" && !isNaN(str)) {
+        string = String(str);
+        return;
+      }
+      string = str || "";
+    },
+    getString: function () {
+      return string;
+    },
 
-  function getString() {
-    return string;
-  }
+    getStringLength: function () {
+      return string.length;
+    },
 
-  function getStringLength() {
-    return string.length;
-  }
-
-  function getReverseString() {
-    return string.split("").reverse().join("");
-  }
-
-  window.module = {setString, getString, getStringLength, getReverseString};
+    getReverseString: function () {
+      return string.split("").reverse().join("");
+    },
+  };
 })();
+
+module.setString("abcde");
+module.getString(); // 'abcde'
+module.getStringLength(); // 5\\
 
 // 5.4
-
-(function () {
+const calculator = (function () {
   let result = 0;
 
-  function setValue(num) {
-    result = num;
-    return this;
-  }
-
-  function plus(num) {
-    result += num;
-    return this;
-  }
-
-  function minus(num) {
-    result -= num;
-    return this;
-  }
-
-  function multiply(num) {
-    result *= num;
-    return this;
-  }
-
-  function divide(num) {
-    result /= num;
-    return this;
-  }
-
-  function power(num) {
-    result = Math.pow(result, num);
-    return this;
-  }
-
-  function getResult() {
-    return result;
-  }
-
-  window.module = {setValue, plus, minus, multiply, divide, power, getResult};
+  return {
+    setValue: function (num) {
+      result = num;
+      return this;
+    },
+    plus: function (num) {
+      result += num;
+      return this;
+    },
+    minus: function (num) {
+      result -= num;
+      return this;
+    },
+    multiply: function (num) {
+      result *= num;
+      return this;
+    },
+    divide: function (num) {
+      result /= num;
+      return this;
+    },
+    power: function (num) {
+      result = Math.pow(result, num);
+      return this;
+    },
+    getResult: function () {
+      return result;
+    },
+  };
 })();
 
-// 6.
+calculator.setValue(10).plus(5).multiply(2).getResult();
 
-const sum = (firstNum) => (secondNum) => (thirdNum) =>
-  firstNum + secondNum + thirdNum;
+// 6.
+const sum = (first) => (second) => (third) => first + second + third;
 
 sum(1)(2)(3); // 6
 sum(2)(3)(4); // 9
